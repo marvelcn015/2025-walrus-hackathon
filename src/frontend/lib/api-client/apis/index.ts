40d907618094c9acb9e1add21ccd4fbfa512ee8c
Aaron Yeh <137107162+marvelcn015@users.noreply.github.com>
2025-11-23 07:48:19 +0800

    feat: API client regeneration and final verification

---
+++ src/frontend/lib/api-client/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DealManagementApi';
+export * from './DealsApi';
+export * from './HealthApi';
+export * from './KPIManagementApi';
+export * from './NautilusApi';
+export * from './SettlementApi';
+export * from './TimelineApi';
+export * from './WalrusApi';
